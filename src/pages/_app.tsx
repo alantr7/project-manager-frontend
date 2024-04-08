a0cce6c4f5c80afcdd59580befaeb1960a082c96
alantr7 <unconfigured@null.spigotmc.org>
2024-04-08 21:15:48 +0200

    Inlined Firebase config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -37,13 +37,13 @@ import Navbar from "@/components/navbar/Navbar";
 import {useLocal} from "@/hooks/useLocal";
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyByURAtf7qlpWAqItqcvdg9ddla0wjhL9I",
+    authDomain: "myqualia-45f29.firebaseapp.com",
+    projectId: "myqualia-45f29",
+    storageBucket: "myqualia-45f29.appspot.com",
+    messagingSenderId: "551038491560",
+    appId: "1:551038491560:web:b1035bcbd96c583b4cdb9d",
+    measurementId: "G-S69W1QP1H7"
 };
 
 export let UserContext = React.createContext({} as User);
